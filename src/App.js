a2fd5b7646e7da5e93bdf2db90fbc50c54b851da
lijeffrey39 <lijeffrey39@gmail.com>
2017-10-27 13:18:25 -0400

    added autocomplete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import AutoComplete from 'material-ui/AutoComplete';
+import MenuItem from 'material-ui/MenuItem';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1> Utterance Editor </h1>
-      </div>
-    );
-  }
-}
+const dataSource3 = [
+  {textKey: 'Some Text', valueKey: 'someFirstValue'},
+  {textKey: 'Some Text', valueKey: 'someSecondValue'},
+];
+const dataSourceConfig = {
+  text: 'textKey',
+  value: 'valueKey',
+};
+
+
+const App = () => (
+	<div className="App">
+		<h1> Utterance Editor </h1>
+		<div>
+			<MuiThemeProvider>
+				<AutoComplete
+					floatingLabelText="Same text, different values"
+					filter={AutoComplete.noFilter}
+					openOnFocus={true}
+					dataSource={dataSource3}
+					dataSourceConfig={dataSourceConfig}
+			    />
+			</MuiThemeProvider>
+		</div>
+    </div>
+);
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1> Utterance Editor </h1>
+//     <AutoComplete
+//       floatingLabelText="Same text, different values"
+//       filter={AutoComplete.noFilter}
+//       openOnFocus={true}
+//       dataSource={dataSource3}
+//       dataSourceConfig={dataSourceConfig}
+//     />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
